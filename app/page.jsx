f0fdd0d77cd5edeed7c4f4861665d996c75aeaed
pevescarol <carol.peves@gmail.com>
2023-11-02 17:29:27 -0300

    se modifica la section dolar

--- app/page.jsx
+++ app/page.jsx
@@ -2,8 +2,34 @@
 import { useState, useEffect } from 'react';
 import { FaMoon, FaSun } from 'react-icons/fa';
 
+import { fetchDolarData } from '../api/dolarApi';
+import DolarCard from "@/components/DolarCard";
+
+import { fetchCryptoExchangeData } from '../api/cryptoApi';
+import CryptoCard from '@/components/CryptoCard';
+
 
 export default function Home() {
+
+  const [dolarOficialData, setDolarOficialData] = useState(null);
+  const [dolarBlueData, setDolarBlueData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const oficialData = await fetchDolarData('oficial');
+        const blueData = await fetchDolarData('blue');
+        setDolarOficialData(oficialData);
+        setDolarBlueData(blueData);
+        setLoading(false);
+      } catch (error) {
+        console.error('Error fetching dollar data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
 
   
   {/* Dark Theme */}
@@ -38,6 +64,13 @@ export default function Home() {
         {darkMode === 'dark' ? <FaSun size={20} /> : <FaMoon size={20} />}
       </button>
       hey
+      <br />
+
+      {/* Dolar Section */}
+      {dolarOficialData && dolarBlueData && (
+          <DolarCard oficialData={dolarOficialData} blueData={dolarBlueData} />
+      )}
+
     </main>
   )
 }
