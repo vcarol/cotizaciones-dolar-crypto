74408e3be30ec9cbe891e85f127b449920e9cf31
pevescarol <carol.peves@gmail.com>
2023-11-02 15:57:19 -0300

    componente Dolar Item

--- components/DolarCard.jsx
+++ components/DolarCard.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
 
-const DolarCard = () => {
+const DolarCard = ({ data }) => {
   return (
-    <div>DolarCard</div>
-  )
-}
+    <div className="grid grid-cols-4 gap-4">
+      <div className="bg-white p-4 rounded-lg shadow-md">
+        <h3>Dólar Oficial</h3>
+        <p>Precio de Compra: ${data.oficial.compra}</p>
+        <p>Precio de Venta: ${data.oficial.venta}</p>
+      </div>
 
-export default DolarCard
+      <div className="bg-white p-4 rounded-lg shadow-md">
+        <h3>Dólar MEP</h3>
+        <p>Precio de Compra: ${data.mep.compra}</p>
+        <p>Precio de Venta: ${data.mep.venta}</p>
+      </div>
+
+      <div className="bg-white p-4 rounded-lg shadow-md">
+        <h3>Dólar Blue</h3>
+        <p>Precio de Compra: ${data.blue.compra}</p>
+        <p>Precio de Venta: ${data.blue.venta}</p>
+      </div>
+
+      <div className="bg-white p-4 rounded-lg shadow-md">
+        <h3>Dólar CCL</h3>
+        <p>Precio de Compra: ${data.ccl.compra}</p>
+        <p>Precio de Venta: ${data.ccl.venta}</p>
+      </div>
+    </div>
+  );
+};
+
+export default DolarCard;
