06ef2d07456296e287d5d85fad28968a8571393f
pevescarol <carol.peves@gmail.com>
2023-11-02 15:58:12 -0300

    componente Crypto Item

--- components/CryptoCard.jsx
+++ components/CryptoCard.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
 
-const CryptoCard = () => {
+const CryptoCard = ({ data }) => {
   return (
-    <div>CryptoCard</div>
-  )
-}
+    <div className="grid grid-cols-4 gap-4">
+      {Object.entries(data).map(([exchange, exchangeData]) => (
+        <div key={exchange} className="bg-white p-4 rounded-lg shadow-md">
+          <h3>{exchange}</h3>
+          <p>Precio de Compra: ${exchangeData.ask}</p>
+          <p>Precio de Venta: ${exchangeData.bid}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
 
-export default CryptoCard
+export default CryptoCard;
