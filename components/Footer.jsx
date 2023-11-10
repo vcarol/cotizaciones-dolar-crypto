57d279e9c32f8d77f6accb3d037fda443cb20f5c
pevescarol <carol.peves@gmail.com>
2023-11-10 15:40:00 -0300

    se agrega componente footer

---
+++ components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className=' dark:bg-[#23232a] dark:text-[#ddd] py-6 '>
+      <div className='max-w-[1140px] mx-auto md:px-8 px-4 text-center text-sm'>
+        © 2023 · Made with ☕️ by ArgenCoin.
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
