3f560879e316ddb41f04e2ed75beccba2f236db2
pevescarol <carol.peves@gmail.com>
2024-01-05 17:45:00 -0300

    se modifica el nombre

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer className=' dark:bg-[#23232a] dark:text-[#ddd] py-6 '>
+    <footer className=' dark:bg-[#23232a] dark:text-[#ddd] py-6 relative bottom-0 '>
       <div className='max-w-[1140px] mx-auto md:px-8 px-4 text-center text-sm'>
-        © 2023 · Made with ☕️ by ArgenCoin.
+        © 2024 · Made with ☕️ by CP.
       </div>
     </footer>
   )
