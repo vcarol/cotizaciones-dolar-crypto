71545d3dea8775dbced9e455fdf8db5d03022efa
pevescarol <carol.peves@gmail.com>
2024-01-05 17:45:38 -0300

    se modifica la api de dolar

--- api/dolarApi.js
+++ api/dolarApi.js
@@ -1,27 +1,27 @@
 import axios from 'axios';
 
-const BASE_URL = 'https://www.dolarsi.com/api/api.php?type=valoresprincipales';
+// const BASE_URL = 'https://www.dolarsi.com/api/api.php?type=valoresprincipales';
 
-export const fetchDolarData = async () => {
+// export const fetchDolarData = async () => {
+//   try {
+//     const response = await axios.get(BASE_URL);
+//     return response.data;
+//   } catch (error) {
+//     console.error('Error fetching dollar data:', error);
+//     return null;
+//   }
+// }
+
+// API para el oficial y el blue:
+const BASE_URL = 'https://dolarapi.com/v1/dolares/'
+
+export const fetchDolarData = async (type) => {
   try {
-    const response = await axios.get(BASE_URL);
+    const response = await axios.get(BASE_URL + type);
     return response.data;
+    
   } catch (error) {
     console.error('Error fetching dollar data:', error);
     return null;
   }
 };
-
-// API para el oficial y el blue:
-// const BASE_URL = 'https://dolarapi.com/v1/dolares/';
-
-// export const fetchDolarData = async (type) => {
-//   try {
-//     const response = await axios.get(BASE_URL + type);
-//     return response.data;
-    
-//   } catch (error) {
-//     console.error('Error fetching dollar data:', error);
-//     return null;
-//   }
-// };
--- app/page.jsx
+++ app/page.jsx
@@ -1,17 +1,20 @@
 'use client'
-import {Open_Sans} from 'next/font/google'
 /* imagenes exchanges */
 import beloImg from '../public/criptos/belo.jpg'
 import lemonImg from '../public/criptos/lemon.jpg'
 import ripioImg from '../public/criptos/ripio.jpg'
 import letsImg from '../public/criptos/lestbit.jpg'
 import buenbitImg from '../public/criptos/buenbit.png'
+import bybitImg from '../public/criptos/bybit.jpg'
+import tiendaImg from '../public/criptos/tiendacrypto.png'
+import bitsoImg from '../public/criptos/bitso-logo.png'
 
 /* iconos selected crypto */
 import usdtIcon from '../public/criptos/tether.svg'
 import usdcIcon from '../public/criptos/usdc.svg'
 import ethIcon from '../public/criptos/ethereum.svg'
 import btcIcon from '../public/criptos/bitcoin.svg'
+import daiIcon from '../public/criptos/dai.svg'
 
 import { useState, useEffect } from 'react';
 import { FaMoon, FaSun } from 'react-icons/fa';
@@ -23,9 +26,6 @@ import { fetchCryptoExchangeData } from '../api/cryptoApi';
 import CryptoCard from '@/components/CryptoCard';
 import Image from 'next/image'
 import Link from 'next/link'
-
-const openSans = Open_Sans({subsets: ["latin"]})
-
 
 export default function Home() {
 
@@ -51,52 +51,62 @@ export default function Home() {
     ripio: ripioImg,
     letsbit: letsImg,
     buenbit: buenbitImg,
+    bitsoalpha: bitsoImg,
+    bybit: bybitImg,
+    tiendacrypto: tiendaImg,
+    ripio: ripioImg,
   }
 
   useEffect(() => {
     // Dolar
     // función para obtener los datos del dolar
     const fetchData = async () => {
-      try {
-        const dolarData = await fetchDolarData()
-        if (dolarData && dolarData.length > 0) {
-          const oficialData = dolarData.find(item => item.casa.nombre === 'Dolar Oficial')
-          const blueData = dolarData.find(item => item.casa.nombre === 'Dolar Blue')
-          const bolsaData = dolarData.find(item => item.casa.nombre === 'Dolar Bolsa')
-          const cclData = dolarData.find(item => item.casa.nombre === 'Dolar Contado con Liqui')
-          setDolarOficialData(oficialData.casa)
-          setDolarBlueData(blueData.casa)
-          setDolarBolsaData(bolsaData.casa)
-          setDolarCclData(cclData.casa)
-          setLoading(false)
-          setLastDolarUpdate(new Date())
-        } else {
-          console.error('No se encontraron datos del dolar.')
-          setLoading(false)
-        }
-      } catch (error) {
-        console.error('Error fetching dollar data:', error)
-        setLoading(false)
-      }
-
-      // llamada anterior, con otra api
       // try {
-      //   const oficialData = await fetchDolarData('oficial')
-      //   const blueData = await fetchDolarData('blue')
-      //   setDolarOficialData(oficialData)
-      //   setDolarBlueData(blueData)
-      //   setLoading(false); // cambia el estado a falso cuando los datos se han cargado
-      //   setLastDolarUpdate(new Date())
+      //   const dolarData = await fetchDolarData()
+      //   if (dolarData && dolarData.length > 0) {
+      //     const oficialData = dolarData.find(item => item.casa.nombre === 'Dolar Oficial')
+      //     const blueData = dolarData.find(item => item.casa.nombre === 'Dolar Blue')
+      //     const bolsaData = dolarData.find(item => item.casa.nombre === 'Dolar Bolsa')
+      //     const cclData = dolarData.find(item => item.casa.nombre === 'Dolar Contado con Liqui')
+      //     setDolarOficialData(oficialData.casa)
+      //     setDolarBlueData(blueData.casa)
+      //     setDolarBolsaData(bolsaData.casa)
+      //     setDolarCclData(cclData.casa)
+      //     setLoading(false)
+      //     setLastDolarUpdate(new Date())
+          
+      //   } else {
+      //     console.error('No se encontraron datos del dolar.')
+      //     setLoading(false)
+      //   }
+        
       // } catch (error) {
       //   console.error('Error fetching dollar data:', error)
-      //   setLoading(false); // es para asegurar que se desactive el estado en caso de error
+      //   setLoading(false)
       // }
+
+      // llamada anterior, con otra api
+      try {
+        const oficialData = await fetchDolarData('oficial')
+        const blueData = await fetchDolarData('blue')
+        const cclData = await fetchDolarData('contadoconliqui')
+        const bolsaData = await fetchDolarData('bolsa')
+        setDolarOficialData(oficialData)
+        setDolarBlueData(blueData)
+        setDolarCclData(cclData)
+        setDolarBolsaData(bolsaData)
+        setLoading(false); // cambia el estado a falso cuando los datos se han cargado
+        setLastDolarUpdate(new Date())
+      } catch (error) {
+        console.error('Error fetching dollar data:', error)
+        setLoading(false); // es para asegurar que se desactive el estado en caso de error
+      }
     }
 
     // función para obtener los datos de criptomonedas
     const fetchCryptoData = async () => {
       try {
-        const exchanges = ['belo', 'lemoncash', 'ripio', 'buenbit', 'letsbit',]
+        const exchanges = ['belo', 'lemoncash', 'ripio', 'buenbit', 'letsbit', 'bitsoalpha', 'bybit', 'tiendacrypto', 'ripio']
         const coin = selectedCrypto; // utiliza la criptomoneda seleccionada
         const fiat = 'ars'
         const volume = 0.1
@@ -109,7 +119,6 @@ export default function Home() {
             link: 'https://www.lemon.me/',
             name: 'Lemon Cash',
           },
-
           ripio: {
             link: 'https://ripio.com/',
             name: 'Ripio',
@@ -121,6 +130,22 @@ export default function Home() {
           buenbit: {
             link: 'https://buenbit.com/',
             name: 'Buenbit',
+          },
+          bitsoalpha: {
+            link: 'https://bitso.com/',
+            name: 'Bitso Alpha',
+          },
+          bybit: {
+            link: 'https://bybit.com/',
+            name: 'Bybit',
+          },
+          tiendacrypto: {
+            link: 'https://tiendacrypto.com/',
+            name: 'TiendaCrypto',
+          },
+          ripio: {
+            link: 'https://ripio.com/',
+            name: 'Ripio',
           },
         };
         
@@ -187,7 +212,7 @@ export default function Home() {
 
 
   return (
-    <main style={openSans.style} className='dark:bg-[#23232a] dark:text-[#ddd] '>
+    <main className='dark:bg-[#23232a] dark:text-[#ddd] border-none '>
       <div className='max-w-[1140px] mx-auto md:px-8 px-4 '>
         <div className='flex flex-col'>
           {/* header */}
@@ -210,7 +235,7 @@ export default function Home() {
             <h1 className='text-[28px] sm:text-3xl md:text-4xl font-semibold'>
               Cotizaciones 💸 <br />al alcance de tu mano
             </h1>
-            <div className="w-full mt-4 mb-8 text-md leading-normal opacity-70 font-medium">
+            <div className="w-full mt-4 mb-8 text-md md:text-lg leading-normal opacity-70 font-medium">
               Sigue las cotizaciones en vivo de las criptomonedas más destacadas,<br className='hidden md:block' /> como Bitcoin, Ethereum y las stablecoins, además del dólar. 
             </div>
           </div>
@@ -255,7 +280,7 @@ export default function Home() {
                 <label htmlFor="cryptoSelect" className="font-bold text-xl md:text-xl">
                   Selecciona una criptomoneda:
                 </label>
-                <div className='flex gap-2 font-bold text-[12px] md:text-lg'>
+                <div className='flex flex-wrap justify-center mx-auto gap-2 font-bold text-base md:text-lg'>
                   <div 
                     className={`flex items-center gap-1 py-2 pl-3 pr-4 rounded-md cursor-pointer ${darkMode === 'dark' ? 'dark:bg-[#131316]' : 'bg-white '} ${selectedCrypto === 'usdt' ? 'selected' : '' }`}
                     onClick={() => handleCrypto('usdt')}
@@ -271,6 +296,14 @@ export default function Home() {
                   >
                     <Image src={usdcIcon} alt='' className='w-5 h-5 md:w-7 md:h-7' />
                     USDC
+                  </div>
+                  <div 
+                    className={`flex items-center gap-1 py-2 pl-3 pr-4 rounded-md cursor-pointer ${darkMode === 'dark' ? 'dark:bg-[#131316]' : 'bg-white '} ${selectedCrypto === 'dai' ? 'selected' : '' }`}
+                    onClick={() => handleCrypto('dai')}
+                    title='Dai'
+                  >
+                    <Image src={daiIcon} alt='' className='w-5 h-5 md:w-7 md:h-7 ' />
+                    DAI
                   </div>
                   <div 
                     className={`flex items-center gap-1 py-2 pl-3 pr-4 rounded-md cursor-pointer ${darkMode === 'dark' ? 'dark:bg-[#131316]' : 'bg-white '} ${selectedCrypto === 'btc' ? 'selected' : '' }`}
@@ -311,13 +344,13 @@ export default function Home() {
         )}
         {/* fecha y data */}
         <div className='flex flex-wrap gap-12 justify-center md:justify-between items-center mt-16 pb-6 ' >
-          <div>
+          <div className='text-center sm:text-left'>
             <p className="text-xs text-gray-500 dark:text-gray-300">
               Última actualización: {lastCryptoUpdate && lastCryptoUpdate.toLocaleString()}
             </p>
             {/* <p className="text-xs text-gray-500 dark:text-gray-300">Se utilizaron APIs de <Link href='https://criptoya.com/' className='text-[#a063f5]'>CriptoYa</Link> y <Link href='https://dolarsi.com/' className='text-[#a063f5]'>DolarSi</Link></p> */}
             <p className="text-xs text-gray-500 dark:text-gray-300">Todos los precios son pasados a $ARS.</p>
-            <p className="text-xs text-gray-500 dark:text-gray-300">Los valores son meramente informativos y <br /> se basan en la información provista por <br /> cada proveedor <Link href='https://criptoya.com/' className='text-[#a063f5]'>CriptoYa</Link> y <Link href='https://dolarsi.com/' className='text-[#a063f5]'>DolarSi</Link>.</p>
+            <p className="text-xs text-gray-500 dark:text-gray-300">Los valores son meramente informativos y <br /> se basan en la información provista por <br /> cada proveedor <Link href='https://criptoya.com/' className='text-[#a063f5]'>CriptoYa</Link> y <Link href='https://dolarapi.com/' className='text-[#a063f5]'>DolarAPI</Link>.</p>
           </div>
 
           <div>
@@ -326,8 +359,9 @@ export default function Home() {
                   src='https://cdn.cafecito.app/imgs/buttons/button_3.png'
                   srcSet='https://cdn.cafecito.app/imgs/buttons/button_3.png  1x, https://cdn.cafecito.app/imgs/buttons/button_3_2x.png 2x, https://cdn.cafecito.app/imgs/buttons/button_3_3.75x.png 3.75x'
                   alt='Invitame un café en cafecito.app'
-                  width={200}  
-                  height={60} 
+                  width={300}  
+                  height={100} 
+                  className='md:w-full h-auto w-[150px] '
                 />
               
             </Link>
